e4231bec6953b7eeff410b60bb38713cfa8faf9c
Divyansh <divyanshch@outlook.com>
2018-11-26 10:20:50 -0600

    updated the logic to calculate discount

--- Cash_Register_Divyansh/BusinessLogic/ItemScannerManager.cs
+++ Cash_Register_Divyansh/BusinessLogic/ItemScannerManager.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Cash_Register_Divyansh.ApplicationTypes;
 using Cash_Register_Divyansh.Contracts.Business;
 using Cash_Register_Divyansh.Models;
+using System;
 
 namespace Cash_Register_Divyansh.BusinessLogic
 {
@@ -13,31 +11,35 @@ namespace Cash_Register_Divyansh.BusinessLogic
         public ListItem ScanAndCalculateCost(Item scannedItem, decimal quantity)
         {
             var lItem = new ListItem();
-            switch (scannedItem.DiscountType)
-            {
-                case DiscountType.Cost:
-                    lItem.CalculatedCost = (100 - Convert.ToDecimal(scannedItem.DiscountValue)) * (decimal)0.01 *
-                                           scannedItem.PerUnitPrice * quantity;
-                    lItem.Item = scannedItem;
-                    lItem.DiscountSummary = string.Format("Discount Applied: {0}% off", scannedItem.DiscountValue);
-                    break;
-                case DiscountType.Quantity:
-                    var baseDiscountedQty = Convert.ToInt16(scannedItem.DiscountValue.Split('|')[0]);
-                    var discountedQty = Convert.ToInt16(scannedItem.DiscountValue.Split('|')[1]);
-                    var calculatedDiscountedQty = ((int)quantity / baseDiscountedQty) * discountedQty;
-                    lItem.CalculatedCost = scannedItem.PerUnitPrice * (quantity - calculatedDiscountedQty);
-                    lItem.Item = scannedItem;
-                    if (calculatedDiscountedQty > 0)
-                    {
-                        lItem.DiscountSummary = string.Format("Discount Applied: Buy {0} get {1} free", baseDiscountedQty, discountedQty);
-                    }
-                    break;
-                default:
-                    lItem.CalculatedCost = scannedItem.PerUnitPrice * quantity;
-                    lItem.Item = scannedItem;
-                    break;
-            }
-            lItem.EnteredQuantity = quantity;
+            
+                switch (scannedItem.DiscountType)
+                {
+                    case DiscountType.Cost:
+                        lItem.CalculatedCost = (100 - Convert.ToDecimal(scannedItem.DiscountValue)) * (decimal)0.01 *
+                                               scannedItem.PerUnitPrice * quantity;
+                        lItem.Item = scannedItem;
+                        lItem.DiscountSummary = string.Format("Discount Applied: {0}% off", scannedItem.DiscountValue);
+                        break;
+                    case DiscountType.Quantity:
+                        var baseQty = Convert.ToInt16(scannedItem.DiscountValue.Split('|')[0]);
+                        var discountedQty = Convert.ToInt16(scannedItem.DiscountValue.Split('|')[1]);
+                        var qtyPerDiscountedQty = (int)(quantity / (baseQty + discountedQty));
+                        lItem.CalculatedCost = qtyPerDiscountedQty * baseQty *
+                                               scannedItem.PerUnitPrice + ((quantity % (baseQty + discountedQty)) * scannedItem.PerUnitPrice);
+                        lItem.Item = scannedItem;
+                        if (qtyPerDiscountedQty > 0)
+                        {
+                            lItem.DiscountSummary = string.Format("Discount Applied: Buy {0} get {1} free", baseQty, discountedQty);
+                        }
+                        break;
+                    default:
+                        lItem.CalculatedCost = scannedItem.PerUnitPrice * quantity;
+                        lItem.Item = scannedItem;
+                        break;
+                }
+                lItem.EnteredQuantity = quantity;
+            
+           
             return lItem;
         }
     }
